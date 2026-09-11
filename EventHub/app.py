c5955a0a28515bf381a8576a77d760338dff7cfa
Luka Ninua <68139639+LuNinua1119@users.noreply.github.com>
2026-09-11 14:08:47 +0400

    Complete EventHub project

--- EventHub/app.py
+++ EventHub/app.py
@@ -40,6 +40,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 login_manager.init_app(app)
 
+with app.app_context()
+    db.create_all()
+
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'warning'
 
